65748e9b7e723958f43bc5e5927791948f760820
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-25 12:02:55 -0400

    Add groovy-language-server

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -79,7 +79,7 @@ return {
 			require('mason-lspconfig').setup_handlers({
 				-- The first entry (without a key) will be the default handler and will be called for each installed server that doesn't have a dedicated handler.
 				function(server_name) -- default handler (optional)
-					require('lspconfig')[server_name].setup{}
+					require('lspconfig')[server_name].setup {}
 				end,
 				-- Disable servers that are set up with a dedicated plugin (they are not initialized here to allow them to be lazy-loaded)
 				['rust_analyzer'] = function()
@@ -115,6 +115,7 @@ return {
 				'rust-analyzer',
 				'json-lsp',
 				'arduino-language-server',
+				'groovy-language-server',
 			},
 		},
 	},
