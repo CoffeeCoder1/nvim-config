c7b0c323d041427a54a1ef064c2f22dfdf44aae0
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-09-22 16:52:26 -0400

    Disable mason-lspconfig
    The setup_handlers thing was changed and needs to be fixed now.

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -76,17 +76,18 @@ return {
 			})
 
 			-- Set up the language servers installed through Mason
-			require('mason-lspconfig').setup_handlers({
-				-- The first entry (without a key) will be the default handler and will be called for each installed server that doesn't have a dedicated handler.
-				function(server_name) -- default handler (optional)
-					require('lspconfig')[server_name].setup {}
-				end,
-				-- Disable servers that are set up with a dedicated plugin (they are not initialized here to allow them to be lazy-loaded)
-				['rust_analyzer'] = function()
-				end,
-				['jdtls'] = function()
-				end,
-			})
+			-- TODO: Get this working again
+			-- require('mason-lspconfig').setup_handlers({
+			-- 	-- The first entry (without a key) will be the default handler and will be called for each installed server that doesn't have a dedicated handler.
+			-- 	function(server_name) -- default handler (optional)
+			-- 		require('lspconfig')[server_name].setup {}
+			-- 	end,
+			-- 	-- Disable servers that are set up with a dedicated plugin (they are not initialized here to allow them to be lazy-loaded)
+			-- 	['rust_analyzer'] = function()
+			-- 	end,
+			-- 	['jdtls'] = function()
+			-- 	end,
+			-- })
 		end,
 	},
 	{
